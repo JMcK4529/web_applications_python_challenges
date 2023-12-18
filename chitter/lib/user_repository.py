29103533c6edeb40bcb1017749ff4a134b400766
JMcK4529 <07josephmckeown@gmail.com>
2023-12-18 13:39:05 +0000

    Test drive UserRepository class

--- chitter/lib/user_repository.py
+++ chitter/lib/user_repository.py
@@ -1,4 +1,5 @@
 from lib.user import User
+from lib.peep import Peep
 
 class UserRepository:
     def __init__(self, connection):
@@ -7,10 +8,68 @@ class UserRepository:
     def all(self):
         """Returns all users without associated peeps."""
         users = []
-        rows = self.connection.execute(
-            """SELECT * FROM users"""
+        rows = self._connection.execute(
+            """SELECT * FROM users;"""
             )
         for row in rows:
             users.append(
                 User(row['id'], row['username'])
             )
+        return users
+    
+    def find(self, id):
+        """Returns the user with id=id."""
+        try:
+            row = self._connection.execute(
+                """
+                SELECT * FROM users WHERE id=%s;
+                """, [id]
+            )[0]
+            return User(row['id'], row['username'])
+        except:
+            raise ValueError(f"User with ID {id} does not exist")
+    
+    def find_with_peeps(self, id):
+        """Returns the user with id=id, including its peeps."""
+        try:
+            user_row = self._connection.execute(
+                """
+                SELECT * FROM users WHERE id=%s;
+                """, [id]
+            )[0]
+            peeps = []
+            peep_rows = self._connection.execute(
+                """
+                SELECT * FROM peeps WHERE user_id=%s;
+                """, [id]
+            )
+            for row in peep_rows:
+                peeps.append(
+                    Peep(row['id'], row['content'],
+                        row['timestamp'], row['user_id'])
+                )
+            return User(user_row['id'], user_row['username'], peeps=peeps)
+        except:
+            raise ValueError(f"User with ID {id} does not exist")
+    
+    def create(self, user):
+        """Inserts a new user into the users table."""
+        self._connection.execute(
+            """
+            INSERT INTO users (username) VALUES (%s)
+            """, [user.username]
+        )
+        row = self._connection.execute(
+            """
+            SELECT * FROM users WHERE id=(SELECT MAX(id) FROM users);
+            """
+        )[0]
+        return User(row['id'], row['username'])
+
+    def delete(self, id):
+        """Deletes a user from the users table."""
+        self._connection.execute(
+            """
+            DELETE FROM users WHERE id=%s;
+            """, [id]
+        )
--- chitter/tests/test_user_repository.py
+++ chitter/tests/test_user_repository.py
@@ -1,18 +1,67 @@
 from lib.user import User
+from lib.peep import Peep
 from lib.user_repository import UserRepository
 from unittest.mock import Mock
+import pytest
+from datetime import datetime
 
 def test_user_repo_constructs():
     try:
         connection = Mock()
         repo = UserRepository(connection)
     except:
-        raise AssertionError("UserRepository class does not construct properly")
+        raise AssertionError(
+            "UserRepository class does not construct properly"
+            )
     
 def test_user_repo_all(db_connection):
     db_connection.seed("seeds/chitter.sql")
     repo = UserRepository(db_connection)
     assert repo.all() == [
-        'User(JMcK4529)'
+        User(1, 'JMcK4529', peeps=[])
     ]
 
+def test_user_repo_find(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repo = UserRepository(db_connection)
+    assert repo.find(1) == User(1, 'JMcK4529', peeps=[])
+    with pytest.raises(Exception) as err:
+        repo.find(2)
+    assert str(err.value) == "User with ID 2 does not exist"
+
+def test_user_repo_find_with_peeps(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repo = UserRepository(db_connection)
+    fwp = repo.find_with_peeps(1)
+    print(type(fwp.peeps[0].timestamp))
+    assert repo.find_with_peeps(1) == \
+        User(
+            1, 'JMcK4529', 
+            peeps=[
+                Peep(
+                    1, 
+                    'Welcome to Chitter!', 
+                    datetime.fromisoformat('2023-12-07 11:13:15'),
+                    1
+                    )
+                ]
+            )
+    with pytest.raises(Exception) as err:
+        repo.find_with_peeps(2)
+    assert str(err.value) == "User with ID 2 does not exist"
+
+def test_user_repo_create(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repo = UserRepository(db_connection)
+    assert repo.create(User(None, 'User2', peeps=None)) == \
+                                        User(2, 'User2', peeps=[])
+
+def test_user_repo_delete(db_connection):
+    db_connection.seed("seeds/chitter.sql")
+    repo = UserRepository(db_connection)
+    assert repo.create(User(None, 'User2', peeps=None)) == \
+                                        User(2, 'User2', peeps=[])
+    assert repo.delete(2) == None
+    assert repo.all() == [
+        User(1, 'JMcK4529', peeps=[])
+    ]
